61470bdf4acd470627468c0ec5c368a047a7cec9
nextaliakbar <stevenaliakabar04@gmail.com>
2025-10-26 19:07:20 +0700

    Update aplikasi v2.5

---
+++ src/constant/AppConstantExample.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package constant;
+
+/**
+ *
+ * @author usER
+ */
+public class AppConstantExample {
+    public final static String FROM = ""; // your email
+    public final static String USERNAME = ""; // your email
+    public final static String PASSWORD = ""; // your password app
+}
